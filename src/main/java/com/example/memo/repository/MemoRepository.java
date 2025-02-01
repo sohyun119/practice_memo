e45819025635100bcec73fd997e8e7afb7c22304
Sohyun <sh-119@naver.com>
2025-02-01 19:22:27 +0900

    Query Method란 무엇일까?

--- src/main/java/com/example/memo/repository/MemoRepository.java
+++ src/main/java/com/example/memo/repository/MemoRepository.java
@@ -4,9 +4,13 @@ import com.example.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 // @Repository -> extends 했기때문에 @Repository 따로 달지 않아도 달아짐
 public interface MemoRepository extends JpaRepository<Memo, Long> {
 
+    List<Memo> findAllByOrderByModifiedAtDesc();
+    // ㄴ *** 메서드 이름 규칙 패턴으로 선언만해도 SQL작성 없이 사용할 수 있다.
 
-
+    //List<Memo> findAllByUserName(String username);
 }
--- src/main/java/com/example/memo/service/MemoService.java
+++ src/main/java/com/example/memo/service/MemoService.java
@@ -39,7 +39,8 @@ public class MemoService {
 
     public List<MemoResponseDto> getMemos() {
 
-        return memoRepository.findAll().stream().map(MemoResponseDto::new).toList();
+        //return memoRepository.findAll().stream().map(MemoResponseDto::new).toList();
+        return memoRepository.findAllByOrderByModifiedAtDesc().stream().map(MemoResponseDto::new).toList();
 
     }
 
